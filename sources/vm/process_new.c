27b04d72c948aa873dd32dc61e0c451473a3fd68
vlaroque <arrageois@gmail.com>
2020-07-11 18:37:50 +0200

    malloc secured

--- sources/vm/process_new.c
+++ sources/vm/process_new.c
@@ -19,7 +19,7 @@ int			new_process(t_data *data, int loc, int champ_id)
 	static int		id = 1;
 
 	if (!(process = (t_process *)malloc(sizeof(t_process))))
-		return (-1);
+		print_error(data, -1);
 	op_bzero(process, sizeof(t_process));
 	process->id = id;
 	id++;
